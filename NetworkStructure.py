fcead43c988762ac074d4722de1b5ad6b92ba750
sakura <526917215@qq.com>
2022-07-17 21:04:22 +0900

    add code GAN structure

---
+++ NetworkStructure.py
@@ -0,0 +1,59 @@
+
+from torch import nn
+
+# Define the network
+class Generator(nn.Module):
+    def __init__(self, image_size, latent_dim, image_channel):
+        super(Generator, self).__init__()
+
+        self.init_size = image_size // 4
+        self.l1 = nn.Sequential(nn.Linear(latent_dim, 128 * self.init_size ** 2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, image_channel, 3, stride=1, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, image_channel):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(image_channel, 16, bn=False),    # batch,14,14
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        # ds_size = image_size // 2 ** 4
+        self.adv_layer = nn.Sequential(nn.Linear(512, 1), nn.Sigmoid())
+
+    def forward(self, img):
+        # img (1024,1,28,28)
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)  # 1024, 512
+        validity = self.adv_layer(out)
+        return validity
